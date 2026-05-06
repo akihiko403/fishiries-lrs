c03882fb861039053e44ab55ce69e8d0acf337b2
lloyd <lloyddongon93@gmail.com>
2026-05-06 15:57:36 +0800

    test3

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('styles.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('styles.css') }}">
 </head>
 <body>
   <div class="shell">
@@ -45,6 +45,6 @@
     </article>
   </template>
 
-  <script src="{{ asset('app.js') }}"></script>
+  <script src="{{ secure_asset('app.js') }}"></script>
 </body>
 </html>
